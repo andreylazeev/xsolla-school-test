9d403c25221033119b4c9f61d190439814f02bcf
andreylazeev <lazeev1998@yandex.ru>
2021-10-26 19:38:07 +0700

    css for select

--- src/components/SelectField/style.ts
+++ src/components/SelectField/style.ts
@@ -4,6 +4,11 @@ export const Select = styled.div`
   display: grid;
   grid-template-columns: max-content 200px;
   grid-column-gap: 20px;
+
+  select {
+    outline: 0;
+    border-radius: 6px;
+  }
 `
 export const Title = styled.span`
   font-family: 'Avenir Medium', sans-serif;
